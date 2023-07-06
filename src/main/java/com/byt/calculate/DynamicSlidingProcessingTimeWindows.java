e7c5a9f02d224876186d5b87b618e7f4cd66ffd8
zhangyf <zhangyfcoder@163.com>
2023-07-06 17:04:28 +0800

    fix bug

--- src/main/java/com/byt/calculate/DynamicSlidingProcessingTimeWindows.java
+++ src/main/java/com/byt/calculate/DynamicSlidingProcessingTimeWindows.java
@@ -7,6 +7,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.WindowAssigner;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.triggers.EventTimeTrigger;
+import org.apache.flink.streaming.api.windowing.triggers.ProcessingTimeTrigger;
 import org.apache.flink.streaming.api.windowing.triggers.Trigger;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 
@@ -19,13 +20,13 @@ import java.util.List;
  * @author: zhangyf
  * @date: 2023/7/4 14:19
  **/
-public class DynamicSlidingEventTimeWindows extends WindowAssigner<Object, TimeWindow> {
+public class DynamicSlidingProcessingTimeWindows extends WindowAssigner<Object, TimeWindow> {
     private static final long serialVersionUID = 1L;
     private final long size;
     private final long offset;
     private final long slide;
 
-    private DynamicSlidingEventTimeWindows(long size, long slide, long offset) {
+    private DynamicSlidingProcessingTimeWindows(long size, long slide, long offset) {
         if (Math.abs(offset) < slide && size > 0L) {
             this.size = size;
             this.slide = slide;
@@ -37,6 +38,7 @@ public class DynamicSlidingEventTimeWindows extends WindowAssigner<Object, TimeW
 
     @Override
     public Collection<TimeWindow> assignWindows(Object element, long timestamp, WindowAssignerContext context) {
+        timestamp = context.getCurrentProcessingTime();
         TagKafkaInfo tagKafkaInfo = null;
         try {
             tagKafkaInfo = (TagKafkaInfo) element;
@@ -45,20 +47,12 @@ public class DynamicSlidingEventTimeWindows extends WindowAssigner<Object, TimeW
         }
         long realSize = timeParams(tagKafkaInfo.getWinSize());
         long realSlide = timeParams(tagKafkaInfo.getWinSlide());
-        if (timestamp > Long.MIN_VALUE) {
-            List<TimeWindow> windows = new ArrayList((int) ((realSize == 0 ? size : realSize) / (realSlide == 0 ? slide : realSlide)));
-            long lastStart = TimeWindow.getWindowStartWithOffset(timestamp, this.offset, (realSlide == 0 ? slide : realSlide));
-
-            for (long start = lastStart; start > timestamp - (realSize == 0 ? size : realSize); start -= (realSlide == 0 ? slide : realSlide)) {
-                windows.add(new TimeWindow(start, start + (realSize == 0 ? size : realSize)));
-            }
-            return windows;
-        } else {
-            throw new RuntimeException(
-                    "Record has Long.MIN_VALUE timestamp (= no timestamp marker). "
-                            + "Is the time characteristic set to 'ProcessingTime', or did you forget to call "
-                            + "'DataStream.assignTimestampsAndWatermarks(...)'?");
+        List<TimeWindow> windows = new ArrayList((int) ((realSize == 0 ? size : realSize) / (realSlide == 0 ? slide : realSlide)));
+        long lastStart = TimeWindow.getWindowStartWithOffset(timestamp, this.offset, (realSlide == 0 ? slide : realSlide));
+        for (long start = lastStart; start > timestamp - (realSize == 0 ? size : realSize); start -= (realSlide == 0 ? slide : realSlide)) {
+            windows.add(new TimeWindow(start, start + (realSize == 0 ? size : realSize)));
         }
+        return windows;
     }
 
 
@@ -72,26 +66,26 @@ public class DynamicSlidingEventTimeWindows extends WindowAssigner<Object, TimeW
 
 
     public String toString() {
-        return "DynamicEventTimeWindows(" + this.size + ", " + this.slide + ")";
+        return "DynamicProcessingTimeWindows(" + this.size + ", " + this.slide + ")";
     }
 
-    public static DynamicSlidingEventTimeWindows of(Time size, Time slide) {
-        return new DynamicSlidingEventTimeWindows(size.toMilliseconds(), slide.toMilliseconds(), 0L);
+    public static DynamicSlidingProcessingTimeWindows of(Time size, Time slide) {
+        return new DynamicSlidingProcessingTimeWindows(size.toMilliseconds(), slide.toMilliseconds(), 0L);
     }
 
-    public static DynamicSlidingEventTimeWindows of() {
+    public static DynamicSlidingProcessingTimeWindows of() {
         // 默认值 数据流中没有开窗参数时使用
-        return new DynamicSlidingEventTimeWindows(5 * 1000L, 5 * 1000L, 0L);
+        return new DynamicSlidingProcessingTimeWindows(5 * 1000L, 5 * 1000L, 0L);
     }
 
-    public static DynamicSlidingEventTimeWindows of(Time size, Time slide, Time offset) {
-        return new DynamicSlidingEventTimeWindows(size.toMilliseconds(), slide.toMilliseconds(), offset.toMilliseconds());
+    public static DynamicSlidingProcessingTimeWindows of(Time size, Time slide, Time offset) {
+        return new DynamicSlidingProcessingTimeWindows(size.toMilliseconds(), slide.toMilliseconds(), offset.toMilliseconds());
     }
 
 
     @Override
     public Trigger<Object, TimeWindow> getDefaultTrigger(StreamExecutionEnvironment env) {
-        return EventTimeTrigger.create();
+        return ProcessingTimeTrigger.create();
     }
 
     @Override
@@ -101,7 +95,7 @@ public class DynamicSlidingEventTimeWindows extends WindowAssigner<Object, TimeW
 
     @Override
     public boolean isEventTime() {
-        return true;
+        return false;
     }
 
     public static Long timeParams(String str) {
