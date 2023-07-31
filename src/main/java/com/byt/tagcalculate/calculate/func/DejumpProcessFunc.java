94910ac56c52ec8c62cc9312dcdadd0e02c39ed2
zyfjrx <zhangyifan0129@gmail.com>
2023-07-31 09:04:59 +0800

    fix bug

--- src/main/java/com/byt/tagcalculate/calculate/func/DejumpProcessFunc.java
+++ src/main/java/com/byt/tagcalculate/calculate/func/DejumpProcessFunc.java
@@ -1,10 +1,14 @@
 package com.byt.tagcalculate.calculate.func;
 
-import com.byt.tagcalculate.pojo.TagKafkaInfo;
 import com.byt.common.utils.BytTagUtil;
+import com.byt.tagcalculate.pojo.TagKafkaInfo;
+import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
@@ -20,6 +24,7 @@ import java.math.BigDecimal;
 public class DejumpProcessFunc extends KeyedProcessFunction<String, TagKafkaInfo, TagKafkaInfo> {
     private OutputTag<TagKafkaInfo> dwdOutPutTag;
     private ValueState<BigDecimal> lastValue;
+    private ValueState<Tuple2<BigDecimal, BigDecimal>> params;
 
     public DejumpProcessFunc(OutputTag<TagKafkaInfo> dwdOutPutTag) {
         this.dwdOutPutTag = dwdOutPutTag;
@@ -33,15 +38,32 @@ public class DejumpProcessFunc extends KeyedProcessFunction<String, TagKafkaInfo
                         Types.BIG_DEC
                 )
         );
+
+        params = getRuntimeContext().getState(
+                new ValueStateDescriptor<Tuple2<BigDecimal, BigDecimal>>(
+                        "params",
+                        TypeInformation.of(new TypeHint<Tuple2<BigDecimal, BigDecimal>>() {
+                        }))
+        );
     }
 
     @Override
     public void processElement(TagKafkaInfo tagKafkaInfo, KeyedProcessFunction<String, TagKafkaInfo, TagKafkaInfo>.Context ctx, Collector<TagKafkaInfo> out) throws Exception {
         BigDecimal upperInt = new BigDecimal(tagKafkaInfo.getUpperInt());
         BigDecimal lowerInt = new BigDecimal(tagKafkaInfo.getLowerInt());
-        if (lastValue.value() != null){
+        if (params.value() == null) {
+            params.update(Tuple2.of(upperInt, lowerInt));
+        } else {
+            if (params.value().f0 != upperInt) {
+                params.value().setField(upperInt, 0);
+            } else if (params.value().f1 != lowerInt) {
+                params.value().setField(lowerInt, 1);
+            }
+            lastValue.clear();
+        }
+        if (lastValue.value() != null) {
             BigDecimal jumpValue = tagKafkaInfo.getValue().subtract(lastValue.value());
-            if (jumpValue.compareTo(lowerInt) == -1 || jumpValue.compareTo(upperInt) == 1){
+            if (jumpValue.compareTo(params.value().f1) == -1 || jumpValue.compareTo(params.value().f0) == 1) {
                 tagKafkaInfo.setValue(lastValue.value());
             } else {
                 lastValue.update(tagKafkaInfo.getValue());
@@ -49,6 +71,6 @@ public class DejumpProcessFunc extends KeyedProcessFunction<String, TagKafkaInfo
         } else {
             lastValue.update(tagKafkaInfo.getValue());
         }
-        BytTagUtil.outputByKeyed(tagKafkaInfo,ctx,out,dwdOutPutTag);
+        BytTagUtil.outputByKeyed(tagKafkaInfo, ctx, out, dwdOutPutTag);
     }
 }
