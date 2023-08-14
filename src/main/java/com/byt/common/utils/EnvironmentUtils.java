e8b11191ccdfa64e295eec3fc7e291830b52f49f
zyfjrx <zhangyifan0129@gmail.com>
2023-08-14 14:02:22 +0800

    动态获取外部配置

--- src/main/java/com/byt/common/utils/EnvironmentUtils.java
+++ src/main/java/com/byt/common/utils/EnvironmentUtils.java
@@ -5,7 +5,12 @@ import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @title: flink读取配置工具类
@@ -13,41 +18,61 @@ import java.io.IOException;
  * @date: 2022/6/21 13:41
  */
 public class EnvironmentUtils {
-
-
-    public static ParameterTool createParameterTool(String[] args) throws IOException {
-        // 系统环境参数 -Dyy=xx
-        ParameterTool systemProperties = ParameterTool.fromSystemProperties();
-
-        // 运行参数 main参数  flink自己数据需要 - 或者 -- 开头做key 例如 -name tom or --name tom
-        ParameterTool fromArgs = ParameterTool.fromArgs(args);
-
-        // 默认配置文件
+    public static ParameterTool createParameterTool() throws IOException {
+        // 获取jar中默认配置
         ParameterTool defaultPropertiesFile = ParameterTool.fromPropertiesFile(
-                EnvironmentUtils.class.getResourceAsStream(PropertiesConstants.FLINK_DEV_PROPERTIES)
+                EnvironmentUtils.class.getClassLoader().getResourceAsStream("application.properties")
         );
 
-        // 合并参数
-        ParameterTool parameterTool = defaultPropertiesFile
-                .mergeWith(fromArgs)
-                .mergeWith(systemProperties);
-        return parameterTool;
+        // 获取外部配置
+        URL url = Test.class.getProtectionDomain().getCodeSource().getLocation();
+        Pattern p = Pattern.compile(".*\\.properties");
+        ParameterTool externalPropertiesFile = null;
+        ParameterTool parameterTool = null;
+        try {
+            String filePath = URLDecoder.decode(url.getPath(), "utf-8");// 转化为utf-8编码，支持中文
+            if (filePath.endsWith(".jar")) {
+                filePath = filePath.substring(0, filePath.lastIndexOf("/") + 1);
+            }
+            File folder = new File(filePath);
+            File[] files = folder.listFiles();
+            if (files == null || files.length == 0) {
+                System.out.println("目标文件夹中没有任何文件");
+                return defaultPropertiesFile;
+            }
+            for (File file : files) {
+                if (!file.isFile()) {
+                    continue;
+                }
+                Matcher matcher = p.matcher(file.getName());
+                if (matcher.matches()) {
+                    externalPropertiesFile = ParameterTool.fromPropertiesFile(file);
+                }
+            }
+
+            parameterTool = defaultPropertiesFile.mergeWith(externalPropertiesFile);
+            return parameterTool;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("外部配置文件异常");
+            return defaultPropertiesFile;
+        }
     }
 
 
-    public static StreamExecutionEnvironment prepareEnv(ParameterTool parameterTool) throws Exception{
+    public static StreamExecutionEnvironment prepareEnv(ParameterTool parameterTool) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // 设置并行度
-        env.setParallelism(parameterTool.getInt(PropertiesConstants.STREAM_PARALLELISM,1));
+        env.setParallelism(parameterTool.getInt(PropertiesConstants.STREAM_PARALLELISM, 1));
 
 
         // ck设置
-        if (parameterTool.getBoolean(PropertiesConstants.STREAM_CHECKPOINT_ENABLE,false)){
+        if (parameterTool.getBoolean(PropertiesConstants.STREAM_CHECKPOINT_ENABLE, false)) {
 
-            env.enableCheckpointing(parameterTool.getLong(PropertiesConstants.STREAM_CHECKPOINT_INTERVAL,10000L));
+            env.enableCheckpointing(parameterTool.getLong(PropertiesConstants.STREAM_CHECKPOINT_INTERVAL, 10000L));
 
             // 设置故障重启策略
-            env.getConfig().setRestartStrategy(RestartStrategies.fixedDelayRestart(4,60000L));
+            env.getConfig().setRestartStrategy(RestartStrategies.fixedDelayRestart(4, 60000L));
 
             // 对 FsStateBackend 刷出去的文件进行文件压缩，减小 checkpoint 体积
             env.getConfig().setUseSnapshotCompression(true);
@@ -58,15 +83,15 @@ public class EnvironmentUtils {
         return env;
     }
 
-    public static StreamExecutionEnvironment prepareWithWebUIEnv(ParameterTool parameterTool) throws Exception{
+    public static StreamExecutionEnvironment prepareWithWebUIEnv(ParameterTool parameterTool) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(parameterTool.getConfiguration());
         // 设置并行度
-        env.setParallelism(parameterTool.getInt(PropertiesConstants.STREAM_PARALLELISM,5));
+        env.setParallelism(parameterTool.getInt(PropertiesConstants.STREAM_PARALLELISM, 5));
         // 设置故障重启策略
-        env.getConfig().setRestartStrategy(RestartStrategies.fixedDelayRestart(4,60000L));
+        env.getConfig().setRestartStrategy(RestartStrategies.fixedDelayRestart(4, 60000L));
         // ck设置
-        if (parameterTool.getBoolean(PropertiesConstants.STREAM_CHECKPOINT_ENABLE,true)){
-            env.enableCheckpointing(parameterTool.getLong(PropertiesConstants.STREAM_CHECKPOINT_INTERVAL,10000L));
+        if (parameterTool.getBoolean(PropertiesConstants.STREAM_CHECKPOINT_ENABLE, true)) {
+            env.enableCheckpointing(parameterTool.getLong(PropertiesConstants.STREAM_CHECKPOINT_INTERVAL, 10000L));
         }
         // 全局配置
         env.getConfig().setGlobalJobParameters(parameterTool);
