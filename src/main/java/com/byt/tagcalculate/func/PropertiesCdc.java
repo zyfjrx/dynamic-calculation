b0968271d43808dbf93ae05092192a14ddbce1ce
zyfjrx <zhangyifan0129@gmail.com>
2023-08-29 16:30:08 +0800

    fix bug

---
+++ src/main/java/com/byt/tagcalculate/func/PropertiesCdc.java
@@ -0,0 +1,33 @@
+package com.byt.tagcalculate.func;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.flink.api.common.state.BroadcastState;
+
+/**
+ * @title:
+ * @author: zhangyifan
+ * @date: 2022/8/13 16:21
+ */
+public interface PropertiesCdc<T> {
+    void broadcastStateAdd(JSONObject jo, BroadcastState<String, T> broadcastState) throws Exception;
+
+    void broadcastStateDel(JSONObject jo, BroadcastState<String, T> broadcastState) throws Exception;
+
+    default void opHandler(JSONObject value, BroadcastState<String, T> broadcastState) throws Exception {
+        JSONObject before = value.getJSONObject("before");
+        JSONObject after = value.getJSONObject("after");
+        String op = value.getString("op");
+        switch (op) {
+            case "u":
+                broadcastStateDel(before, broadcastState);
+                broadcastStateAdd(after, broadcastState);
+                break;
+            case "d":
+                broadcastStateDel(before, broadcastState);
+                break;
+            default:
+                broadcastStateAdd(after, broadcastState);
+        }
+    }
+
+}
